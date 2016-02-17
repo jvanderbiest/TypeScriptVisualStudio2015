69b79c4d2d2448a11412b1cdfffb6a05ea6c2fb8
Jonas Van der Biest <jvanderbiest@gmail.com>
2016-02-17 09:56:05 +0100

    initial

---
+++ src/typescript/gulpfile.js
@@ -0,0 +1,30 @@
+﻿/// <binding AfterBuild='libs' Clean='clean' />
+/*
+This file in the main entry point for defining Gulp tasks and using Gulp plugins.
+Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
+*/
+
+var gulp = require('gulp');
+var rimraf = require('rimraf');
+
+var paths = {
+    npm: './node_modules/',
+    lib: './wwwroot/lib/'
+}
+
+var libs = [
+   paths.npm + 'es6-shim/es6-shim.min.js',
+   paths.npm + 'systemjs/dist/system-polyfills.js',
+   paths.npm + 'angular2/bundles/angular2-polyfills.js',
+   paths.npm + 'systemjs/dist/system.src.js',
+   paths.npm + 'rxjs/bundles/Rx.js',
+   paths.npm + 'angular2/bundles/angular2.dev.js'
+];
+
+gulp.task('libs', function () {
+    return gulp.src(libs).pipe(gulp.dest(paths.lib));
+});
+
+gulp.task('clean', function (callback) {
+    rimraf(paths.lib, callback);
+});
---
+++ src/typescript/scripts/app/main.ts
@@ -0,0 +1,6 @@
+﻿///<reference path="../../node_modules/angular2/typings/browser.d.ts"/>
+
+import {AppComponent} from './app.component'
+import {bootstrap}    from 'angular2/platform/browser'
+
+bootstrap(AppComponent);
